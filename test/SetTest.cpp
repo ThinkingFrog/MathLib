35a809f6df585dc4fbac5b09feccd7e1c6563a27
ThinkingFrog <khripunkovdv@gmail.com>
2021-08-29 17:53:35 +0300

    Add new testing macroses

--- test/SetTest.cpp
+++ test/SetTest.cpp
@@ -34,101 +34,78 @@ void SetTest::testGetSize() {
     CLEAR_SET_ONE
 }
 void SetTest::testGetCopy() {
-    CREATE_LOGGER
-    CREATE_SET_ONE
-    CREATE_VEC_ONE
-    CREATE_VEC_TWO
+    CREATE_ALL
 
     RC err;
     IVector* tmp;
     
-    err = set1->insert(vec1, IVector::NORM::FIRST, TOLERANCE);
+    err = set1->insert(vec1, DEFAULT_NORM, TOLERANCE);
     assert(err == RC::SUCCESS);
-    err = set1->insert(vec2, IVector::NORM::FIRST, TOLERANCE);
+    err = set1->insert(vec2, DEFAULT_NORM, TOLERANCE);
     assert(err == RC::SUCCESS);
 
     err = set1->getCopy(0, tmp);
     assert(err == RC::SUCCESS);
-    assert(IVector::equals(vec1, tmp, IVector::NORM::FIRST, TOLERANCE));
+    assert(IVector::equals(vec1, tmp, DEFAULT_NORM, TOLERANCE));
     delete tmp;
     
     err = set1->getCopy(1, tmp);
     assert(err == RC::SUCCESS);
-    assert(IVector::equals(vec2, tmp, IVector::NORM::FIRST, TOLERANCE));
+    assert(IVector::equals(vec2, tmp, DEFAULT_NORM, TOLERANCE));
     delete tmp;
 
-    CLEAR_VEC_TWO
-    CLEAR_VEC_ONE
-    CLEAR_SET_ONE
-    CLEAR_LOGGER
+    CLEAR_ALL
 }
 void SetTest::testGetCoords() {
-    CREATE_LOGGER
-    CREATE_SET_ONE
-    CREATE_VEC_ONE
-    CREATE_VEC_TWO
+    CREATE_ALL
 
     RC err;
 
-    err = set1->insert(vec1, IVector::NORM::FIRST, TOLERANCE);
+    err = set1->insert(vec1, DEFAULT_NORM, TOLERANCE);
     assert(err == RC::SUCCESS);
     
     err = set1->getCoords(0, vec2);
     assert(err == RC::SUCCESS);
-    assert(IVector::equals(vec1, vec2, IVector::NORM::FIRST, TOLERANCE));
+    assert(IVector::equals(vec1, vec2, DEFAULT_NORM, TOLERANCE));
 
-    CLEAR_VEC_TWO
-    CLEAR_VEC_ONE
-    CLEAR_SET_ONE
-    CLEAR_LOGGER
+    CLEAR_ALL
 }
 
 void SetTest::testInsert() {
-    CREATE_LOGGER
-    CREATE_SET_ONE
-    CREATE_VEC_ONE
-    CREATE_VEC_TWO
-    CREATE_VEC_THREE
+    CREATE_ALL
 
     RC err;
 
-    err = set1->insert(vec1, IVector::NORM::FIRST, TOLERANCE);
+    err = set1->insert(vec1, DEFAULT_NORM, TOLERANCE);
     assert(err == RC::SUCCESS);
     assert(set1->getSize() == 1);
     assert(set1->getDim() == vec1->getDim());
 
-    err = set1->insert(vec2, IVector::NORM::FIRST, TOLERANCE);
+    err = set1->insert(vec2, DEFAULT_NORM, TOLERANCE);
     assert(err == RC::SUCCESS);
     assert(set1->getSize() == 2);
     assert(set1->getDim() == vec1->getDim());
 
-    err = set1->insert(vec2, IVector::NORM::FIRST, TOLERANCE);
+    err = set1->insert(vec2, DEFAULT_NORM, TOLERANCE);
     assert(err == RC::VECTOR_ALREADY_EXIST);
     assert(set1->getSize() == 2);
     assert(set1->getDim() == vec1->getDim());
 
-    err = set1->insert(vec3, IVector::NORM::FIRST, TOLERANCE);
+    err = set1->insert(vec3, DEFAULT_NORM, TOLERANCE);
     assert(err == RC::MISMATCHING_DIMENSIONS);
     assert(set1->getSize() == 2);
     assert(set1->getDim() == vec1->getDim());
 
-    CLEAR_VEC_ONE
-    CLEAR_VEC_TWO
-    CLEAR_VEC_THREE
-    CLEAR_SET_ONE
-    CLEAR_LOGGER
+    CLEAR_ALL
 }
 void SetTest::testRemoveByIndex() {
-    CREATE_LOGGER
-    CREATE_SET_ONE
-    CREATE_VEC_ONE
-    CREATE_VEC_TWO
+    CREATE_ALL
 
     RC err;
 
-    err = set1->insert(vec1, IVector::NORM::FIRST, TOLERANCE);
+    err = set1->insert(vec1, DEFAULT_NORM, TOLERANCE);
     assert(err == RC::SUCCESS);
-    err = set1->insert(vec2, IVector::NORM::FIRST, TOLERANCE);
+    err = set1->insert(vec2, DEFAULT_NORM, TOLERANCE);
     assert(err == RC::SUCCESS);
 
     err = set1->remove(1);
@@ -139,13 +116,31 @@ void SetTest::testRemoveByIndex() {
     assert(err = RC::INDEX_OUT_OF_BOUND);
     assert(set1->getSize() == 1);
 
-    CLEAR_VEC_TWO
-    CLEAR_VEC_ONE
-    CLEAR_SET_ONE
-    CLEAR_LOGGER
+    CLEAR_ALL
 }
-void SetTest::testRemoveByPattern() {}
+void SetTest::testRemoveByPattern() {
+    CREATE_ALL
 
+    RC err;
+
+    err = set1->insert(vec1, DEFAULT_NORM, TOLERANCE);
+    assert(err == RC::SUCCESS);
+    err = set1->insert(vec2, DEFAULT_NORM, TOLERANCE);
+    assert(err == RC::SUCCESS);
+
+    err = set1->remove(vec2, DEFAULT_NORM, TOLERANCE);
+    assert(err = RC::SUCCESS);
+    assert(set1->getSize() == 1);
+
+    err = set1->remove(vec1, DEFAULT_NORM, TOLERANCE);
+    assert(err = RC::SUCCESS);
+    assert(set1->getSize() == 0);
+
+    err = set1->remove(vec1, DEFAULT_NORM, TOLERANCE);
+    assert(err = RC::SOURCE_SET_EMPTY);
+
+    CLEAR_ALL
+}
 
 void SetTest::testClone() {
     CREATE_LOGGER
@@ -154,7 +149,7 @@ void SetTest::testClone() {
 
     RC err;
 
-    err = set1->insert(vec1, IVector::NORM::FIRST, TOLERANCE);
+    err = set1->insert(vec1, DEFAULT_NORM, TOLERANCE);
     assert(err == RC::SUCCESS);
     ISet* set2 = set1->clone();
 
@@ -165,8 +160,51 @@ void SetTest::testClone() {
     CLEAR_LOGGER
 }
 
-void SetTest::testFindFirst() {}
-void SetTest::testFindFirstAndCopy() {}
+void SetTest::testFindFirst() {
+    CREATE_ALL
+
+    RC err;
+
+    err = set1->insert(vec1, DEFAULT_NORM, TOLERANCE);
+    assert(err == RC::SUCCESS);
+
+    err = set1->insert(vec2, DEFAULT_NORM, TOLERANCE);
+    assert(err == RC::SUCCESS);
+
+    err = set1->findFirst(vec1, DEFAULT_NORM, TOLERANCE);
+    assert(err == RC::SUCCESS);
+
+    err = set1->findFirst(nullptr, DEFAULT_NORM, TOLERANCE);
+    assert(err == RC::NULLPTR_ERROR);
+
+    err = set1->findFirst(vec3, DEFAULT_NORM, TOLERANCE);
+    assert(err == RC::MISMATCHING_DIMENSIONS);
+
+    err = set1->remove(vec2, DEFAULT_NORM, TOLERANCE);
+    assert(err == RC::SUCCESS);
+
+    err = set1->findFirst(vec2, DEFAULT_NORM, TOLERANCE);
+    assert(err == RC::VECTOR_NOT_FOUND);
+
+    CLEAR_ALL
+}
+void SetTest::testFindFirstAndCopy() {
+    CREATE_ALL
+
+    IVector* test;
+    RC err;
+
+    err = set1->insert(vec1, DEFAULT_NORM, TOLERANCE);
+    assert(err = RC::SUCCESS);
+    err = set1->insert(vec2, DEFAULT_NORM, TOLERANCE);
+    assert(err == RC::SUCCESS);
+
+    err = set1->findFirstAndCopy(vec2, DEFAULT_NORM, TOLERANCE, test);
+    assert(err == RC::SUCCESS);
+    assert(IVector::equals(test, vec2, DEFAULT_NORM, TOLERANCE));
+
+    CLEAR_ALL
+}
 void SetTest::testFindFirstAndCopyCoords() {}
 
 void SetTest::testMakeIntersection() {}
--- test/tests.hpp
+++ test/tests.hpp
@@ -5,19 +5,21 @@
 #include "ISet.h"
 
 #define TOLERANCE 1e-6
+#define DEFAULT_NORM IVector::NORM::SECOND
 
 #define CREATE_LOGGER ILogger* logger = ILogger::createLogger(); IVector::setLogger(logger); ISet::setLogger(logger); 
-#define CREATE_SET_ONE ISet* set1 = ISet::createSet();
 #define CREATE_VEC_ONE std::array<double, 4> data1({1, 5.5, 6, 8.5}); IVector* vec1 = IVector::createVector(data1.size(), data1.data());
 #define CREATE_VEC_TWO std::array<double, 4> data2({5, 3, 9, 7}); IVector* vec2 = IVector::createVector(data2.size(), data2.data());
 #define CREATE_VEC_THREE std::array<double, 3> data3({1, 10, 100}); IVector* vec3 = IVector::createVector(data3.size(), data3.data());
+#define CREATE_SET_ONE ISet* set1 = ISet::createSet();
+#define CREATE_ALL CREATE_LOGGER CREATE_VEC_ONE CREATE_VEC_TWO CREATE_VEC_THREE CREATE_SET_ONE
 
 #define CLEAR_VEC_ONE delete vec1;
 #define CLEAR_VEC_TWO delete vec2;
 #define CLEAR_VEC_THREE delete vec3;
 #define CLEAR_SET_ONE delete set1;
 #define CLEAR_LOGGER delete logger;
-
+#define CLEAR_ALL CLEAR_LOGGER CLEAR_VEC_ONE CLEAR_VEC_TWO CLEAR_VEC_THREE CLEAR_SET_ONE
 
 namespace VecTest {
     void testCreate();
