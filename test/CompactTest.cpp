b3e2a82b00b38250bb5f2f66cf253c46b4fba2c9
ThinkingFrog <khripunkovdv@gmail.com>
2021-09-09 19:38:12 +0300

    Add basic compact tests

--- test/CompactTest.cpp
+++ test/CompactTest.cpp
@@ -1,7 +1,92 @@
 #include "tests.hpp"
+#include <cassert>
 #include <iostream>
+
+void CompactTest::testCreate() {
+    CREATE_ALL
+
+    assert(com1 != nullptr);
+
+    CLEAR_ALL
+}
+void CompactTest::testClone() {
+    CREATE_ALL
+    ICompact *tmp = com1->clone();
+
+    assert(tmp != nullptr);
+
+    delete tmp;
+    CLEAR_ALL
+}
+
+void CompactTest::testGetDim() {
+    CREATE_ALL
+
+    assert(com1->getDim() == vec1->getDim());
+
+    CLEAR_ALL
+}
+void CompactTest::testGetGrid() {
+    CREATE_ALL
+    IMultiIndex *tmp = com1->getGrid();
+
+    const size_t *tmp_data = tmp->getData();
+    const size_t *index_data = index1->getData();
+    for (size_t idx = 0; idx < com1->getDim(); ++idx)
+        assert(tmp_data[idx] == index_data[idx]);
+
+    delete tmp;
+    CLEAR_ALL
+}
+void CompactTest::testGetLeftBoundary() {
+    CREATE_ALL
+    IVector *left = nullptr;
+
+    RC err = com1->getLeftBoundary(left);
+    assert(err == RC::SUCCESS);
+    assert(IVector::equals(vec1, left, DEFAULT_NORM, TOLERANCE));
+
+    delete left;
+    CLEAR_ALL
+}
+void CompactTest::testGetRightBoundary() {
+    CREATE_ALL
+    IVector *right = nullptr;
+
+    RC err = com1->getRightBoundary(right);
+    assert(err == RC::SUCCESS);
+    assert(IVector::equals(vec2, right, DEFAULT_NORM, TOLERANCE));
+
+    delete right;
+    CLEAR_ALL
+}
+void CompactTest::testGetVectorCoords() {
+    CREATE_ALL
+    double *empty_data = new double[com1->getDim()];
+    IVector *tmp = IVector::createVector(com1->getDim(), empty_data);
+
+    //    RC err = com1->getVectorCoords();
+
+    delete[] empty_data;
+    delete tmp;
+    CLEAR_ALL
+}
+void CompactTest::testGetVectorCopy() {}
+
+void CompactTest::testIsInside() {}
 
 void CompactTest::testAll() {
     std::cout << "Running all Compact tests" << std::endl;
+
+    testCreate();
+    testClone();
+    testGetDim();
+    testGetGrid();
+    testGetLeftBoundary();
+    testGetRightBoundary();
+    testGetVectorCoords();
+    testGetVectorCopy();
+    testIsInside();
+
     std::cout << "Successfully ran all Compact tests" << std::endl;
 }
--- test/tests.hpp
+++ test/tests.hpp
@@ -12,7 +12,8 @@
     ILogger *logger = ILogger::createLogger();                                                                         \
     IVector::setLogger(logger);                                                                                        \
     ISet::setLogger(logger);                                                                                           \
-    IMultiIndex::setLogger(logger);
+    IMultiIndex::setLogger(logger);                                                                                    \
+    ICompact::setLogger(logger);
 #define CREATE_VEC_ONE                                                                                                 \
     std::array<double, 4> data1({1, 5.5, 6, 8.5});                                                                     \
     IVector *vec1 = IVector::createVector(data1.size(), data1.data());
@@ -34,9 +35,10 @@
 #define CREATE_INDEX_TWO                                                                                               \
     std::array<size_t, 4> idata2({12, 76, 44, 89});                                                                    \
     IMultiIndex *index2 = IMultiIndex::createMultiIndex(idata2.size(), idata2.data());
+#define CREATE_COM_ONE ICompact *com1 = ICompact::createCompact(vec1, vec2, index1);
 #define CREATE_ALL                                                                                                     \
     CREATE_LOGGER CREATE_VEC_ONE CREATE_VEC_TWO CREATE_VEC_THREE CREATE_SET_ONE CREATE_SET_TWO CREATE_SET_THREE        \
-        CREATE_VEC_FOUR CREATE_INDEX_ONE CREATE_INDEX_TWO
+        CREATE_VEC_FOUR CREATE_INDEX_ONE CREATE_INDEX_TWO CREATE_COM_ONE
 
 #define CLEAR_LOGGER delete logger;
 #define CLEAR_VEC_ONE delete vec1;
@@ -48,9 +50,10 @@
 #define CLEAR_SET_THREE delete set3;
 #define CLEAR_INDEX_ONE delete index1;
 #define CLEAR_INDEX_TWO delete index2;
+#define CLEAR_COM_ONE delete com1;
 #define CLEAR_ALL                                                                                                      \
     CLEAR_LOGGER CLEAR_VEC_ONE CLEAR_VEC_TWO CLEAR_VEC_THREE CLEAR_SET_ONE CLEAR_SET_TWO CLEAR_SET_THREE               \
-        CLEAR_VEC_FOUR CLEAR_INDEX_ONE CLEAR_INDEX_TWO
+        CLEAR_VEC_FOUR CLEAR_INDEX_ONE CLEAR_INDEX_TWO CLEAR_COM_ONE
 
 namespace VecTest {
 void testCreate();
@@ -137,10 +140,9 @@ void testGetDim();
 void testGetGrid();
 void testGetLeftBoundary();
 void testGetRightBoundary();
+
 void testGetVectorCoords();
 void testGetVectorCopy();
-
-void testIncAxisIndex();
 void testIsInside();
 
 void testAll();
